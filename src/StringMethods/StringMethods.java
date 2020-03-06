a573ed2db06243f3a5981388b7a28a1a8886a80c
League Student <league@iMac-28.attlocal.net>
2020-03-05 18:28:16 -0800

    Working on string methods

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -7,7 +7,8 @@ import java.util.Base64;
 Visit the JavaDocs for the String class to view everything you can do with a String.  
 
 
-HINT:  Here are some String methods you might find useful 
+HINT:  Here are some String methods you might find useful
+charAt 
 contains
 replace
 trim
@@ -32,33 +33,77 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+
+		if (s1.length() > s2.length()) {
+			return s1;
+		} else {
+			return s2;
+		}
 	}
 
-	
-	// if String s contains the word "underscores", change all of the spaces to underscores
+	// if String s contains the word "underscores", change all of the spaces to
+	// underscores
 	public static String formatSpaces(String s) {
-		return null;
+		if (s.contains("underscores")) {
+			s = s.replace(" ", "_");
+
+		}
+		return s;
 	}
 
-	
-	// Return the name of the person whose LAST name would appear first if they were in alphabetical order
+	// Return the name of the person whose LAST name would appear first if they were
+	// in alphabetical order
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
-		return null;
+		s1 = s1.trim();
+		String[] a = s1.split(" ");
+		s2 = s2.trim();
+		String[] b = s2.split(" ");
+		s3 = s3.trim();
+		String[] c = s3.split(" ");
+		int f = a[1].compareToIgnoreCase(b[1]);
+		int g = a[1].compareToIgnoreCase(c[1]);
+		int x = b[1].compareToIgnoreCase(a[1]);
+		int y = b[1].compareToIgnoreCase(c[1]);
+		int t = c[1].compareToIgnoreCase(a[1]);
+		int j = c[1].compareToIgnoreCase(b[1]);
+		if (f < 0 && g < 0) {
+
+			return s1;
+		}else if(x<0&&y<0) {
+			return s2;
+		}else {
+			return s3;
+		}
+		
 	}
-	
-	
+
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+		int total=0;
+		for (int i = 0; i < s.length(); i++) {
+			char e=s.charAt(i);
+			if(Character.isDigit(e)==true) {
+				int w=Integer.parseInt(e+"");
+				total+=w;
+			}
+		}
+		return total;
 	}
-	
-	
+
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
+		for (int i = 0; i < s.length(); i++) {
+			int amountOfTimes=0;
+			if(s.contains(substring)) {
+				amountOfTimes++;
+			}else {
+				return 0;
+			}
+		}
 		return 0;
+		
 	}
 
 	// Call Utitilities.encrypt to encrypt String s
@@ -71,13 +116,11 @@ public class StringMethods {
 		return null;
 	}
 
-
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
 		return 0;
 	}
-	
 
 	// Given String s, return the number of characters between the first occurrence
 	// of String substring and the final occurrence
@@ -86,14 +129,13 @@ public class StringMethods {
 		return 0;
 	}
 
-
 	// Return true if String s is a palindrome
 	// palindromes are words or phrases are read the same forward as backward.
 	// HINT: ignore/remove all punctuation and spaces in the String
 	public static boolean palindrome(String s) {
 		return true;
 	}
-	
+
 }
 
 class Utilities {
